7098f88553b058929feb54fb1e9d63c55533d419
S5201967 <91668770+S5201967@users.noreply.github.com>
2021-11-15 00:29:25 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,13 +3,16 @@ using namespace std;
 int main() {
     int n=0 , x=1, y=1, z=0;
     cin>>n;
+    if(n>0){
+        cout<<x<<endl;
+    }
     while(x<=n){
-        x=y+z;
-        z=y;
-        y=x;
-        if(x<=n){
-            cout<<x<<endl;
-        }
+            x=y+z;
+            z=y;
+            y=x;
+            if(x<=n){
+                cout<<x<<endl;
+            }
     }
     return 0;
 }
